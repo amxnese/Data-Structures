c62bd0e5702f1b427c2a725d44534d0e963cf154
amine <aminesdrt@gmail.com>
2024-02-01 15:07:51 +0100

    Added a Priority Queue Class To The 'Queue.py' File

--- Queue.py
+++ Queue.py
@@ -47,3 +47,40 @@ class Queue():
             while(temp):
                 print(temp.data)
                 temp = temp.next
+
+' The Least The Value The Highest The Priority '
+class PriorityQueue(Queue): 
+    def __init__(self):
+        super().__init__()
+        self.head = None
+
+    def enqueue(self, data):
+        newNode = Node(data)
+        # Handling The Case Where The Queue is Empty
+        if self.is_empty():
+          self.head = newNode
+          return
+        # Handling The Case Where The New Element Has The Highest Priority
+        if self.head.data > data:
+          newNode.next = self.head
+          self.head = newNode
+          return
+
+        prv = self.head
+        temp = prv.next 
+        '''if The Queue Contain Only One Element temp Will Be None, 
+        That's Why We'll Check That it's Not None in The While Loop'''
+        while temp and temp.next and temp.data < data:
+          prv = prv.next
+          temp = temp.next
+          
+        'Now We Need To Know Why We Broke Out of The Loop'
+        
+        # The Case Where We Reached The End of The Queue And The New Element Has The Least Priority
+        if temp and not temp.next and temp.data < data:
+            temp.next = newNode
+        
+        # The Case Where The New Element Found a Place in The Middle of The Queue
+        else:
+          prv.next = newNode
+          newNode.next = temp
